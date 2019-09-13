44bfd299f2f90a617bb1a139f8482306c2b79a78
Samantha Haviland <shaviland@live.com>
2019-09-13 17:26:25 -0600

    Create README.md

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
 
 public class MakeChangeApp {
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		double priceOwed = 34.50;
+		double tenderPaid = 40.00;
+		double roundedPenny = 0.005;
+		
+		
+		double changeCalc = (tenderPaid - priceOwed + roundedPenny);
+		System.out.println(changeCalc);
+		
+		int changeBreak = (int)(changeCalc * 100);
+		System.out.println(changeBreak);
+		
+		
+	}
 
 }
