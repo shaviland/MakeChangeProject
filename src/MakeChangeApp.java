a025ef1f629cac4d6d4776c74ae042244b034321
Samantha Haviland <shaviland@live.com>
2019-09-13 17:33:59 -0600

    read me

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class MakeChangeApp {
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
-		double priceOwed = 34.50;
+		double priceOwed = 22.50;
 		double tenderPaid = 40.00;
 		double roundedPenny = 0.005;
 		
@@ -11,10 +11,13 @@ public class MakeChangeApp {
 		double changeCalc = (tenderPaid - priceOwed + roundedPenny);
 		System.out.println(changeCalc);
 		
-		int changeBreak = (int)(changeCalc * 100);
-		System.out.println(changeBreak);
+		int pennies = (int)(changeCalc * 100);
+		System.out.println(pennies);
 		
-		
+		int bills = pennies / 100;
+		System.out.println(bills);
+		int changeRem = pennies % 100;
+		System.out.println(changeRem);
 	}
 
 }
