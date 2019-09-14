d96525152825447ce90373159b4a9ab027ed5dd0
Samantha Haviland <shaviland@live.com>
2019-09-14 13:16:30 -0600

    MVP

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -3,45 +3,72 @@ import java.util.Scanner;
 public class MakeChangeApp {
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
-		double priceOwed = 22.37;
-		double tenderPaid = 40.00;
+		double priceOwed = 0.0;
+		double tenderPaid = 0.0;
 		double roundedPenny = 0.005;
-		
-		
+
+		System.out.println("Please input amount owed: ");
+		priceOwed = kb.nextDouble();
+
+		System.out.println("Please input amount paid: ");
+		tenderPaid = kb.nextDouble();
+
+		if (tenderPaid < priceOwed) {
+			System.err.println("Not enough tender. Please pay the amount owed. $" + priceOwed);
+		}else {
+			System.out.println("The change is:");
+		}
+
 		double changeCalc = (tenderPaid - priceOwed + roundedPenny);
-		System.out.println(changeCalc);
-		
-		int penniesStart = (int)(changeCalc * 100);
-		System.out.println(penniesStart);
-		
+//		System.out.println(changeCalc);
+
+		int penniesStart = (int) (changeCalc * 100);
+//		System.out.println(penniesStart);		
+
 		int bills = penniesStart / 100;
 //		System.out.println("bills" + bills);
 		int twenties = bills / 20;
-		System.out.println("twenties " + twenties);
+		if (twenties > 0) {
+			System.out.println("twenties: " + twenties);
+		}
 		int billsNext = bills % 20;
-		System.out.println("bills next " + billsNext);
+//		System.out.println("bills next " + billsNext);
 		int tens = billsNext / 10;
-		System.out.println("tens " + tens);
+		if (tens > 0) {
+			System.out.println("tens: " + tens);
+		}
 		int billsRem = billsNext % 10;
 //		System.out.println("bills rem " + billsRem);
 		int fives = billsRem / 5;
-		System.out.println("fives " + fives);
+		if (fives > 0) {
+			System.out.println("fives: " + fives);
+		}
 		int ones = billsRem % 5;
-		System.out.println("ones " + ones);
+		if (ones > 0) {
+			System.out.println("ones: " + ones);
+		}
 		int coins = penniesStart % 100;
 //		System.out.println("coins " + coins);
 		int quarters = coins / 25;
-		System.out.println("quarters " + quarters);
+		if (quarters > 0) {
+			System.out.println("quarters: " + quarters);
+		}
 		int changeRem = coins % 25;
 //		System.out.println("change rem " + changeRem);
 		int dimes = changeRem / 10;
-		System.out.println("dimes " + dimes);
+		if (dimes > 0) {
+			System.out.println("dimes: " + dimes);
+		}
 		int moreCoins = changeRem % 10;
 //		System.out.println("more coins " + moreCoins);
 		int nickles = moreCoins / 5;
-		System.out.println("nickles " + nickles);
+		if (nickles > 0) {
+			System.out.println("nickles: " + nickles);
+		}
 		int penniesFinal = moreCoins % 5;
-		System.out.println("pennies " + penniesFinal);
+		if (penniesFinal > 0) {
+			System.out.println("pennies: " + penniesFinal);
+		}
 	}
 
 }
